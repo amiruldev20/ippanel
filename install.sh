10a2c7b4f8b5f8734b7c880b234e27d4fee95219
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-04-03 01:33:30 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# AUTO INSTALL DPANELCP
+echo "installing nvm.."
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+echo "add path nvm.."
+source ~/.profile
+
+echo "installing pm2..."
+npm i pm2
+echo "add path pm2..."
+export PATH=$PATH:~/node_modules/pm2/bin
+echo "done!!"
