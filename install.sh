a541d2d553227dd49c4475c4512c9f8ea9e8cd5b
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-04-03 01:59:33 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,14 +4,10 @@
 echo "installing nvm.."
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 echo "add path nvm.."
-source ~/.profile
-
+source ~/.nvm/nvm.sh
 echo "installing nodejs V19.8.1..."
 nvm i v19.8.1
 nvm install --tls
-
 echo "installing pm2..."
-npm i pm2
-echo "add path pm2..."
-export PATH=$PATH:~/node_modules/pm2/bin
+npm i pm2 -g
 echo "done!!"
