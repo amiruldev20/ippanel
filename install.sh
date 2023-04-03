13d70320d44ad4e9df69743d3ce3e041d32f33b9
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-04-03 02:22:07 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
-#!/bin/bash
-
 # AUTO INSTALL DPANELCP
 echo "installing nvm.."
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 echo "add path nvm.."
+source ~/.profile
 source ~/.nvm/nvm.sh
 echo "installing nodejs V19.8.1..."
 nvm i v19.8.1
 nvm install --tls
 echo "installing pm2..."
-npm i pm2 -g
+npm i pm2
 echo "done!!"
