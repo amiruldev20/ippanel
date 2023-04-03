915ed4bd45c5be3dcd23ddc8819860a7cb0db82d
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-04-03 01:54:14 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 echo "add path nvm.."
 source ~/.profile
 
+echo "installing nodejs V19.8.1..."
+nvm i v19.8.1
+nvm install --tls
+
 echo "installing pm2..."
 npm i pm2
 echo "add path pm2..."
