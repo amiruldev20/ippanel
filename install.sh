935ec916c7a4370c2e0a5cadf45f23289dab276c
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-06-01 08:27:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,14 @@ echo "installing nvm.."
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 echo "add path nvm.."
 source ~/.profile
-source ~/.nvm/nvm.sh
 echo "installing nodejs V19.8.1..."
 nvm i v19.8.1
 nvm install --tls
 echo "installing pm2..."
 npm i pm2
+echo "export path pm2"
+mkdir ~/.bin
+ln -s ~/node_modules/pm2/bin/pm2 ~/bin/pm2
+echo 'export PATH=$PATH:~/bin' >> ~/.bashrc
+source ~/.bashrc
 echo "done!!"
