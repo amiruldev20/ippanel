1622ce9da03e3ed08fa41ced7b55864121a28e82
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-06-30 07:53:36 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,16 +8,8 @@ source ~/.profile
 
 echo "Installing Node.js v19.8.1..."
 nvm install v19.8.1
-nvm use v19.8.1
-nvm install --latest-npm
 
 echo "Installing pm2..."
 npm install pm2 -g
-
-echo "Exporting pm2 path..."
-mkdir -p ~/.bin
-ln -s ~/node_modules/pm2/bin/pm2 ~/.bin/pm2
-echo 'export PATH=$PATH:~/.bin' >> ~/.bashrc
-source ~/.bashrc
 
 echo "Installation completed!"
