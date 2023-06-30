bdeea3309693184f735b5769b10cf9cf1d976b43
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-06-30 10:35:55 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,11 @@
 
 echo "Installing nvm..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-source ~/.bashrc
+source ~/.nvm/nvm.sh
 
 echo "Installing Node.js v19.8.1..."
 nvm install v19.8.1
+nvm use v19.8.1
 
 echo "Installing pm2..."
 npm install pm2 -g
