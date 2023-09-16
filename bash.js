dc20210e83751b484563469dbe0d90fdcf1822f5
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-09-17 06:05:54 +0700

    Update bash.js

--- bash.js
+++ bash.js
@@ -1,18 +1,5 @@
 var bs = require('child_process')
 process.env.TZ = 'Asia/Jakarta'
-function deleteConfigFolder() {
-  bs.exec('rm -rf .cache .pki', (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-     // console.error(`stderr: ${stderr}`);
-      return;
-    }
-   // console.log(`Config folder deleted successfully.`);
-  });
-}
 
 bs.spawn('bash', [], {
   stdio: ['inherit', 'inherit', 'inherit', 'ipc']
