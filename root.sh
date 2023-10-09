ccb7f4312ae758737c2b3d7bf8a07b3bf2af64e9
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-10-09 12:38:58 +0700

    Create root.sh

---
+++ root.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Mod By SL
+#echo "$crot    ALL=(ALL:ALL) ALL" >> /etc/sudoers;
+wget -qO- -O /etc/ssh/sshd_config https://raw.githubusercontent.com/amiruldev20/ippanel/main/sshd_config;
+systemctl restart sshd;
+clear;
+echo -e "Masukkan Password:";
+read -e pwe;
+usermod -p `perl -e "print crypt("$pwe","Q4")"` root;
+clear;
+printf "Mohon Simpan Informasi Akun VPS Ini
+============================================
+Akun Root (Akun Utama)
+Ip address = $(curl -Ls http://ipinfo.io/ip)
+Username   = root
+Password   = $pwe
+============================================
+echo "";
+exit;
