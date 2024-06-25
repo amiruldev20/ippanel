042655ca8b333d1bbe620d196fced2119440105c
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2024-06-25 03:26:49 +0000

    Update autoroot.sh

--- autoroot.sh
+++ autoroot.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 # mod by amiruldev
-wget -qO- -O /etc/ssh/sshd_config https://raw.githubusercontent.com/amiruldev20/ippanel/main/sshd_configs
-systemctl restart sshd;
-clear;
-echo -e "Masukkan Password:";
-read -e pwe;
-usermod -p `perl -e "print crypt("$pwe","Q4")"` root;
-clear;
-printf "Mohon Simpan Informasi Akun VPS Ini
-============================================
-Akun Root (Akun Utama)
-Ip address = $(curl -Ls http://ipinfo.io/ip)
-Username   = root
-Password   = $pwe
-============================================
-echo "";
-exit;
+
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Script ini harus dijalankan sebagai root."
+  exit 1
+fi
+
+wget -qO /etc/ssh/sshd_config https://raw.githubusercontent.com/amiruldev20/ippanel/main/sshd_configs
+systemctl restart sshd
+clear
+
+echo -e "Masukkan Password baru root:"
+read -s pwe
+# Ubah password root
+usermod -p "$(openssl passwd -1 "$pwe")" root
+clear
+ip_address=$(curl -Ls http://ipinfo.io/ip)
+
+# Tampilkan informasi akun
+printf "Mohon Simpan Informasi Akun VPS Ini\n"
+printf "============================================\n"
+printf "Akun Root (Akun Utama)\n"
+printf "Ip address = %s\n" "$ip_address"
+printf "Username   = root\n"
+printf "Password   = %s\n" "$pwe"
+printf "============================================\n"
+echo ""
+
+exit
